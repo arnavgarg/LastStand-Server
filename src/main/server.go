d64d4d29642c133784bce8a594a7f34bc2cb38b6
Arnav Garg <arnavga@gmail.com>
2018-05-12 13:28:33 -0700

    added server response

--- src/main/server.go
+++ src/main/server.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"os"
 	"net"
+	"io"
+	"time"
 )
 
 func main() {
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		fmt.Println("[  ERROR  ] Failed to Listen on Port 8080")
+		fmt.Println("[  ERROR  ] Failed to Listen on Port 8080\n" + err.Error())
 		os.Exit(3)
 	}
 	defer ln.Close()
@@ -18,8 +20,11 @@ func main() {
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			fmt.Println("[ ERROR ] Failed to Accept Incoming Connections")
+			fmt.Println("[  ERROR  ] Failed to Accept Incoming Connections\n" + err.Error())
 		}
+		fmt.Println("[ SUCCESS ] Connection from " + conn.RemoteAddr().String())
+		io.WriteString(conn, fmt.Sprint("[ SUCCESS ] Connection established at ", time.Now() , "\n"))
+
 		go handleConnection(conn)
 	}
 }
