18eac0e34cb89bad39b355e130120c575dcaacf5
Arnav Garg <arnavga@gmail.com>
2018-05-29 14:25:43 -0700

    changed from POST to PUT

--- src/main/server.go
+++ src/main/server.go
@@ -28,7 +28,7 @@ func main() {
 		fmt.Println("[ SUCCESS ] New Player Joined from ", r.RemoteAddr)
 	}).Methods("PUT");
 
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/player", func(w http.ResponseWriter, r *http.Request) {
 		var data ChangeData
 		err := json.NewDecoder(r.Body).Decode(&data)
 		if (err != nil) {
@@ -37,9 +37,9 @@ func main() {
 		defer r.Body.Close()
 
 		game.ApplyChanges(data)
-		fmt.Println("[ SUCCESS ] Changes Applied from ", r.RemoteAddr)
 		json.NewEncoder(w).Encode(game.GetGameData())
-	}).Methods("POST")
+		fmt.Println("[ SUCCESS ] Changes Applied from ", r.RemoteAddr)
+	}).Methods("PUT")
 
 	startGame()
 
